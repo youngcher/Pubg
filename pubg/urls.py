e87261b0834afd84056629b512a9f9e48f87b510
Hello97KDY <hello99887@naver.com>
2023-07-07 17:06:18 +0900

    Kim config

--- pubg/urls.py
+++ pubg/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from pubg import views
 
+
 urlpatterns = [
     path('', views.index),
     path('show_test/', views.show_test, name='show_test'),
--- pubg/views.py
+++ pubg/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 import pandas as pd
+from pubg.Map_service import get_player_data,get_match_telemetries,get_positions_draw,main
+
 
 
 data = {'Col1':[1,2,3], 'Col2':['A','B','C']}
@@ -12,11 +14,11 @@ def index(request):
     return HttpResponse(df)
 
 def show_test(request):
-    df_html = df.to_html()
+    # df_html = df.to_html()
 
-    context = {'my_var':my_var, 'df_html':df_html}
+    # context = {'my_var':my_var, 'df_html':df_html}
 
-    return render(request, "test.html", context)
+    return render(request ,main() )
 
 def read(request, id):
     return HttpResponse('Read!'+id)
